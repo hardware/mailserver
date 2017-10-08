hardware/mailserver

commit 71bf851f1a19c9667641ffdb7e4812d3a3ccb014

feat(): allow to disable the local dns resolver #174

Makefile
@@ -69,6 +69,7 @@ init:
 		-e DISABLE_SIGNING=true \
 		-e DISABLE_GREYLISTING=true \
 		-e DISABLE_RATELIMITING=true \
+		-e DISABLE_DNS_RESOLVER=true \
 		-e ENABLE_POP3=true \
 		-e ENABLE_ENCRYPTION=true \
 		-e OPENDKIM_KEY_LENGTH=4096 \

rootfs/usr/local/bin/run.sh
@@ -18,6 +18,7 @@ export CERTFILE
 export KEYFILE
 export FULLCHAIN
 export DISABLE_CLAMAV
+export DISABLE_DNS_RESOLVER
 export RECIPIENT_DELIMITER
 export FETCHMAIL_INTERVAL
 export RELAY_NETWORKS
@@ -41,6 +42,7 @@ DISABLE_SIEVE=${DISABLE_SIEVE:-false}
 DISABLE_SIGNING=${DISABLE_SIGNING:-false}
 DISABLE_GREYLISTING=${DISABLE_GREYLISTING:-false}
 DISABLE_RATELIMITING=${DISABLE_RATELIMITING:-false}
+DISABLE_DNS_RESOLVER=${DISABLE_DNS_RESOLVER:-false}
 ENABLE_POP3=${ENABLE_POP3:-false}
 ENABLE_FETCHMAIL=${ENABLE_FETCHMAIL:-false}
 ENABLE_ENCRYPTION=${ENABLE_ENCRYPTION:-false}
@@ -351,6 +353,18 @@ else
   echo "[INFO] POP3 protocol is disabled"
 fi
 
+# Disable Unbound DNS resolver
+if [ "$DISABLE_DNS_RESOLVER" = true ]; then
+  echo "[INFO] Unbound DNS resolver is disabled"
+  # Disable DNSSEC in Postfix and Rspamd configuration
+  sed -i -e 's|\(enable_dnssec.*=\).*|\1 false;|' \
+         -e '/nameserver/ s/^/#/' /etc/rspamd/local.d/options.inc
+  sed -i -e 's|\(smtp_tls_security_level.*=\).*|\1 may|' \
+         -e '/smtp_dns_support_level/ s/^/#/' /etc/postfix/main.cf
+else
+  echo "[INFO] Unbound DNS resolver is enabled"
+fi
+
 if [ "$TESTING" = true ]; then
   echo "[INFO] DOCKER IMAGE UNDER TESTING"
   # Disable postfix virtual table
@@ -498,18 +512,22 @@ rm -f /var/mail/dovecot/instances
 # UNBOUND
 # ---------------------------------------------------------------------------------------------
 
-# Get a copy of the latest root DNS servers list
-curl -s -o /etc/unbound/root.hints https://www.internic.net/domain/named.cache > /dev/null
+if [ "$DISABLE_DNS_RESOLVER" = false ]; then
 
-# Update the root trust anchor to perform cryptographic DNSSEC validation
-unbound-anchor -a /etc/unbound/root.key
+  # Get a copy of the latest root DNS servers list
+  curl -s -o /etc/unbound/root.hints https://www.internic.net/domain/named.cache > /dev/null
 
-# Setting up unbound-control
-unbound-control-setup &> /dev/null
+  # Update the root trust anchor to perform cryptographic DNSSEC validation
+  unbound-anchor -a /etc/unbound/root.key
 
-# Set permissions
-chmod 775 /etc/unbound
-chown -R unbound:unbound /etc/unbound
+  # Setting up unbound-control
+  unbound-control-setup &> /dev/null
+
+  # Set permissions
+  chmod 775 /etc/unbound
+  chown -R unbound:unbound /etc/unbound
+
+fi
 
 # RSPAMD
 # ---------------------------------------------------------------------------------------------

rootfs/services/unbound/run
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Use the local DNS server
-echo "nameserver 127.0.0.1" > /etc/resolv.conf
+if [ "$DISABLE_DNS_RESOLVER" = true ]; then
+  cp -f /etc/resolv.conf /var/mail/postfix/spool/etc/resolv.conf
+  exit 0
+fi
 
-# Update resolv.conf in postfix queue
-cp -f /etc/resolv.conf /var/mail/postfix/spool/etc/resolv.conf
+# Use the local DNS server
+echo "nameserver 127.0.0.1" | tee /etc/resolv.conf \
+                                  /var/mail/postfix/spool/etc/resolv.conf \
+                                  >/dev/null
 
 logger -p mail.info "s6-supervise : spawning unbound process"
 exec unbound &> /dev/stdout

test/tests.bats
@@ -191,7 +191,7 @@ load 'test_helper/bats-assert/load'
   run docker exec mailserver_reverse /bin/bash -c "ps aux --forest | grep '[s]6-supervise unbound'"
   assert_success
   run docker exec mailserver_reverse /bin/bash -c "ps aux --forest | grep -v 's6' | grep '[u]nbound'"
-  assert_success
+  assert_failure
 }
 
 #
@@ -288,7 +288,7 @@ load 'test_helper/bats-assert/load'
 
 @test "checking port    (53): internal port listening (reverse configuration)" {
   run docker exec mailserver_reverse /bin/sh -c "nc -z 127.0.0.1 53"
-  assert_success
+  assert_failure
 }
 
 @test "checking port   (110): external port closed    (default configuration)" {
@@ -378,7 +378,7 @@ load 'test_helper/bats-assert/load'
 
 @test "checking port  (8953): internal port listening (reverse configuration)" {
   run docker exec mailserver_reverse /bin/sh -c "nc -z 127.0.0.1 8953"
-  assert_success
+  assert_failure
 }
 
 @test "checking port (10026): internal port listening (default configuration)" {
@@ -785,6 +785,30 @@ load 'test_helper/bats-assert/load'
   assert_output "postdrop"
 }
 
+@test "checking postfix: smtp_tls_security_level value (default configuration)" {
+  run docker exec mailserver_default postconf -h smtp_tls_security_level
+  assert_success
+  assert_output "dane"
+}
+
+@test "checking postfix: smtp_tls_security_level value (reverse configuration)" {
+  run docker exec mailserver_reverse postconf -h smtp_tls_security_level
+  assert_success
+  assert_output "may"
+}
+
+@test "checking postfix: smtp_dns_support_level value (default configuration)" {
+  run docker exec mailserver_default postconf -h smtp_dns_support_level
+  assert_success
+  assert_output "dnssec"
+}
+
+@test "checking postfix: smtp_dns_support_level value (reverse configuration)" {
+  run docker exec mailserver_reverse postconf -h smtp_dns_support_level
+  assert_success
+  assert_output ""
+}
+
 #
 # dovecot
 #
@@ -967,25 +991,47 @@ load 'test_helper/bats-assert/load'
 # unbound
 #
 
-@test "checking unbound: /etc/resolv.conf" {
+@test "checking unbound: /etc/resolv.conf (default configuration)" {
   run docker exec mailserver_default cat /etc/resolv.conf
   assert_success
   assert_output "nameserver 127.0.0.1"
 }
 
-@test "checking unbound: /var/mail/postfix/spool/etc/resolv.conf" {
+@test "checking unbound: /etc/resolv.conf (reverse configuration)" {
+  run docker exec mailserver_reverse cat /etc/resolv.conf
+  assert_success
+  refute_output "nameserver 127.0.0.1"
+}
+
+@test "checking unbound: /var/mail/postfix/spool/etc/resolv.conf (default configuration)" {
   run docker exec mailserver_default cat /var/mail/postfix/spool/etc/resolv.conf
   assert_success
   assert_output "nameserver 127.0.0.1"
 }
 
-@test "checking unbound: root.hints exist" {
+@test "checking unbound: /var/mail/postfix/spool/etc/resolv.conf (reverse configuration)" {
+  run docker exec mailserver_reverse cat /var/mail/postfix/spool/etc/resolv.conf
+  assert_success
+  refute_output "nameserver 127.0.0.1"
+}
+
+@test "checking unbound: root.hints exist (default configuration)" {
   run docker exec mailserver_default [ -f /etc/unbound/root.hints ]
   assert_success
 }
 
-@test "checking unbound: root.key exist" {
+@test "checking unbound: root.hints doesn't exist (reverse configuration)" {
+  run docker exec mailserver_reverse [ ! -f /etc/unbound/root.hints ]
+  assert_success
+}
+
+@test "checking unbound: root.key exist (default configuration)" {
   run docker exec mailserver_default [ -f /etc/unbound/root.key ]
+  assert_success
+}
+
+@test "checking unbound: root.key doesn't exist (reverse configuration)" {
+  run docker exec mailserver_reverse [ ! -f /etc/unbound/root.key ]
   assert_success
 }
 

commit 108f30eb2d6787edc13d81d31888f3ed16cc10a1

fix(): run.sh hanging
I added some quotes to avoid run.sh script hanging when one of these variables is not defined.

+ fix a serious bug introduced in 71bf851 commit. When unbound is disabled, sometime parent process wait indefinitely.

Makefile
@@ -56,10 +56,11 @@ init:
 		--link redis:redis \
 		-e FQDN=mail.domain.tld \
 		-e DOMAIN=domain.tld \
-		-e DBPASS=testpasswd \
+		-e DBPASS=/tmp/passwd/mariadb \
 		-e REDIS_HOST=redis \
 		-e REDIS_PORT=6379 \
-		-e RSPAMD_PASSWORD=testpasswd \
+		-e REDIS_PASS=/tmp/passwd/redis \
+		-e RSPAMD_PASSWORD=/tmp/passwd/rspamd \
 		-e VMAILUID=`id -u` \
 		-e VMAILGID=`id -g` \
 		-e VMAIL_SUBDIR=subdir \
@@ -75,6 +76,7 @@ init:
 		-e OPENDKIM_KEY_LENGTH=4096 \
 		-e TESTING=true \
 		-v "`pwd`/test/share/tests":/tmp/tests \
+		-v "`pwd`/test/share/passwd":/tmp/passwd \
 		-v "`pwd`/test/share/ssl/rsa":/var/mail/ssl \
 		-v "`pwd`/test/share/sieve/custom.sieve":/var/mail/sieve/custom.sieve \
 		-v "`pwd`/test/share/letsencrypt":/etc/letsencrypt \

rootfs/services/_parent/run
@@ -12,7 +12,7 @@
 # 8. clamd     (antivirus, is launched after database update)
 
 s6-svc -u /services/rsyslogd && s6-svwait -u /services/rsyslogd
-s6-svc -u /services/unbound  && s6-svwait -u /services/unbound
+s6-svc -u /services/unbound  && [ "$DISABLE_DNS_RESOLVER" = true ] || s6-svwait -u /services/unbound
 s6-svc -u /services/postfix  && s6-svwait -u /services/postfix
 s6-svc -u /services/dovecot
 s6-svc -u /services/rspamd

rootfs/usr/local/bin/run.sh
@@ -33,12 +33,12 @@ DBHOST=${DBHOST:-mariadb}
 DBPORT=${DBPORT:-3306}
 DBNAME=${DBNAME:-postfix}
 DBUSER=${DBUSER:-postfix}
-DBPASS=$([ -f $DBPASS ] && cat $DBPASS || echo ${DBPASS:-})
+DBPASS=$([ -f "$DBPASS" ] && cat "$DBPASS" || echo "${DBPASS:-}")
 REDIS_HOST=${REDIS_HOST:-redis}
 REDIS_PORT=${REDIS_PORT:-6379}
-REDIS_PASS=$([ -f $REDIS_PASS ] && cat $REDIS_PASS || echo ${REDIS_PASS:-})
+REDIS_PASS=$([ -f "$REDIS_PASS" ] && cat "$REDIS_PASS" || echo "${REDIS_PASS:-}")
 REDIS_NUMB=${REDIS_NUMB:-0}
-RSPAMD_PASSWORD=$([ -f $RSPAMD_PASSWORD ] && cat $RSPAMD_PASSWORD || echo ${RSPAMD_PASSWORD:-})
+RSPAMD_PASSWORD=$([ -f "$RSPAMD_PASSWORD" ] && cat "$RSPAMD_PASSWORD" || echo "${RSPAMD_PASSWORD:-}")
 DISABLE_RSPAMD_MODULE=${DISABLE_RSPAMD_MODULE:-}
 DISABLE_CLAMAV=${DISABLE_CLAMAV:-false}
 DISABLE_SIEVE=${DISABLE_SIEVE:-false}
@@ -552,7 +552,7 @@ adduser --quiet \
         _rspamd
 
 # Setting the controller password
-PASSWORD=$(rspamadm pw --quiet --encrypt --type pbkdf2 --password ${RSPAMD_PASSWORD})
+PASSWORD=$(rspamadm pw --quiet --encrypt --type pbkdf2 --password "${RSPAMD_PASSWORD}")
 sed -i "s|<PASSWORD>|${PASSWORD}|g" /etc/rspamd/local.d/worker-controller.inc
 
 # Set permissions

test/share/passwd/mariadb
@@ -1,0 +1,1 @@
+testpasswd

test/share/passwd/rspamd
@@ -1,0 +1,1 @@
+testpasswd